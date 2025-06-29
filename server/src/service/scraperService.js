b6907b6009f25e3aa046154c27233f448a57e6ab
Rajnish kumar <rajnishmaurya250@gmail.com>
2025-06-29 11:36:31 +0530

    puppter__

--- server/src/service/scraperService.js
+++ server/src/service/scraperService.js
@@ -84,7 +84,7 @@ export const scrapeNaukri = async (query, location) => {
   //       ? process.env.PUPPETEER_EXECUTABLE_PATH
   //       : puppeteer.executablePath(),
   // });
-    const browser = await puppeteer.launch({executablePath: '/path/to/Chrome'});
+    const browser = await puppeteer.launch({executablePath: 'usr/bin/google-chrome-stable'});
   const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 30000 });
